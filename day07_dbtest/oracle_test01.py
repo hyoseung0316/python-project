465f84deeaef603b7cd8ad583443d0f95ba2c65c
hyoseung0316 <kaitokid4468@gmail.com>
2022-05-23 14:51:44 +0900

    2022/5/23 오라클 라이브러리 설치 내용 추가

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -1,8 +1,9 @@
+# pip install ex_Oracle
 import cx_Oracle
 
 conn = cx_Oracle.connect('scott','tiger','192.168.1.17:1521/XE')
 cursor = conn.cursor()
-cursor.execute("select * from emp")
+cursor.execute("select * from emp where deptno = 10")
 
 for item in cursor:
     print(item[1],item[5])
@@ -10,12 +11,3 @@ for item in cursor:
 conn.close()
 
 
-while True:
-    choice=input('''
-다음 중에서 하실 일을 골라주세요:
-1. 사원 검색 2. 사원 찾기
->>>''').upper()
-    if choice == "1":
-        while True:
-            print('job을 입력해주세요')
-            input('job')
